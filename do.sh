fdd853bffd6c4a657003ea9fb59159b82dcccb7e
hekan0508 <1078522562@qq.com>
2016-04-19 18:56:24 +0800

    新增上传脚本

--- do.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.email "1078522562@qq.com"
-git config --global user.name "hekan0508"
-git config --global push.default simple
-git add .
-git commit -m $/
-git push
-start http://hekan0508.github.io
